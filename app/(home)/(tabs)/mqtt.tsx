1b57edb70f61e9a9c2cf66fce62d10fce4003069
Erik <erik.pustoslemsek@gmail.com>
2024-06-04 18:50:47 +0200

    KAN-31 Page init

---
+++ app/(home)/(tabs)/mqtt.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import * as Paho from 'paho-mqtt';
+
+const MqttPage = () => {
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
+      Mqtt
+    </View>
+  );
+};
+
+export default MqttPage;
